ebeefd62b1f8c3a4ea3f9eaad7efffefac76763d
vothanhvanpro <55737088+vothanhvanpro@users.noreply.github.com>
2019-09-24 15:46:52 +0700

    Khởi tạo  cơ sở dữ liệu, tạo form

---
+++ QuanLyDiemSinhVien/QuanLyDiemSinhVien/BSlayer/BLDangNhap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using QuanLyDiemSinhVien.DBlayer;
+
+namespace QuanLyDiemSinhVien.BSlayer
+{
+    class BLDangNhap
+    {
+        public static string username;
+        public static string password;
+        DBMain db = null;
+        public BLDangNhap()
+        {
+            db = new DBMain();
+        }
+        //Tạo tài khoản mới
+        public bool ThemUser(string UserName, string Password, ref string err)
+        {
+            string sqlString = "Insert Into tblUser(ID,Password,PhanQuyen) Values(" + "'" +
+                UserName + "','" +
+                Password + "','3')";
+            return db.MyExecuteNonQuery(sqlString, CommandType.Text, ref err);
+        }
+        //Lấy thông tin tài khoản đã có trong bảng SQL để đăng nhập
+        public DataTable LayUser(string UserName, string Password, ref string err)
+        {
+            string sqlString = "Select * from tblUser where ID='" + UserName + "' and Password = '" + Password
+                + "'";
+            return db.GetDataTable(sqlString, CommandType.Text, ref err);
+        }
+    }
+}
---
+++ QuanLyDiemSinhVien/QuanLyDiemSinhVien/frmDangNhap.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace QuanLyDiemSinhVien
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDangNhap));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.txtReMatKhauDK = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMatKhauDK = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTaiKhoanDK = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbDangNhap = new System.Windows.Forms.LinkLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnHuy);
+            this.groupBox2.Controls.Add(this.btnDangKy);
+            this.groupBox2.Controls.Add(this.txtReMatKhauDK);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtMatKhauDK);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtTaiKhoanDK);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 370);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(398, 281);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Đăng ký";
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHuy.ImageKey = "1460546211_DeleteRed.ico";
+            this.btnHuy.ImageList = this.imageList1;
+            this.btnHuy.Location = new System.Drawing.Point(258, 220);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(134, 46);
+            this.btnHuy.TabIndex = 3;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnHuy.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1460546187_tick.ico");
+            this.imageList1.Images.SetKeyName(1, "1460546211_DeleteRed.ico");
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDangKy.ImageIndex = 0;
+            this.btnDangKy.ImageList = this.imageList1;
+            this.btnDangKy.Location = new System.Drawing.Point(113, 220);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(134, 46);
+            this.btnDangKy.TabIndex = 2;
+            this.btnDangKy.Text = "Đăng ký";
+            this.btnDangKy.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            // 
+            // txtReMatKhauDK
+            // 
+            this.txtReMatKhauDK.Location = new System.Drawing.Point(113, 150);
+            this.txtReMatKhauDK.Name = "txtReMatKhauDK";
+            this.txtReMatKhauDK.PasswordChar = '*';
+            this.txtReMatKhauDK.Size = new System.Drawing.Size(279, 20);
+            this.txtReMatKhauDK.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Nhập lại:";
+            // 
+            // txtMatKhauDK
+            // 
+            this.txtMatKhauDK.Location = new System.Drawing.Point(113, 101);
+            this.txtMatKhauDK.Name = "txtMatKhauDK";
+            this.txtMatKhauDK.PasswordChar = '*';
+            this.txtMatKhauDK.Size = new System.Drawing.Size(279, 20);
+            this.txtMatKhauDK.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Mật Khẩu:";
+            // 
+            // txtTaiKhoanDK
+            // 
+            this.txtTaiKhoanDK.Location = new System.Drawing.Point(113, 52);
+            this.txtTaiKhoanDK.Name = "txtTaiKhoanDK";
+            this.txtTaiKhoanDK.Size = new System.Drawing.Size(279, 20);
+            this.txtTaiKhoanDK.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Tài Khoản:";
+            // 
+            // lbDangNhap
+            // 
+            this.lbDangNhap.AutoSize = true;
+            this.lbDangNhap.Location = new System.Drawing.Point(120, 261);
+            this.lbDangNhap.Name = "lbDangNhap";
+            this.lbDangNhap.Size = new System.Drawing.Size(121, 13);
+            this.lbDangNhap.TabIndex = 4;
+            this.lbDangNhap.TabStop = true;
+            this.lbDangNhap.Text = "Bạn chưa có tài khoản?";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.lbDangNhap);
+            this.groupBox1.Controls.Add(this.btnDangNhap);
+            this.groupBox1.Controls.Add(this.txtPass);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, -8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(398, 298);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Đăng nhập";
+            // 
+            // btnExit
+            // 
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.ImageKey = "1460546211_DeleteRed.ico";
+            this.btnExit.ImageList = this.imageList1;
+            this.btnExit.Location = new System.Drawing.Point(257, 196);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(134, 46);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDangNhap.ImageIndex = 0;
+            this.btnDangNhap.ImageList = this.imageList1;
+            this.btnDangNhap.Location = new System.Drawing.Point(100, 196);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(134, 46);
+            this.btnDangNhap.TabIndex = 3;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(113, 101);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(279, 20);
+            this.txtPass.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mật Khẩu:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(113, 53);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(279, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tài Khoản:";
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 643);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmDangNhap";
+            this.Text = "frmDangNhap";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.TextBox txtReMatKhauDK;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMatKhauDK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTaiKhoanDK;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel lbDangNhap;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+    }
+}
